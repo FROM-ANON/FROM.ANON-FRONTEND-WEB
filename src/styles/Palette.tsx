fdfca2e050c572c87a58c9f11fd051515886b54f
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-11-16 16:49:16 +0900

    feat: 색상 추가 및 변경

--- src/styles/Palette.tsx
+++ src/styles/Palette.tsx
@@ -3,13 +3,15 @@ export const Palette = {
     Sub: { orange: "#FF7A00" },
 
     White: "#FFFFFF",
-    Gray05: "#F9F9F9",
+    Gray05: "#F3F3F3",
     Gray10: "#E6E6E6",
     Gray20: "#CCCCCC",
     Gray30: "#B3B3B3",
     Gray40: "#999999",
     Gray50: "#7F7F7F",
     Gray60: "#666666",
+    Gray70: "#4D4A45",
+    Gray80: "#33312E",
     Black: "#000000",
 
     Mandarin05: "#FFFAF2",
