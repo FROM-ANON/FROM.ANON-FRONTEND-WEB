6af2936d1d29996fe6b978e36782bbf7e3b151ec
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-11-16 16:50:06 +0900

    fix: Link UI 수정

--- src/components/Main/Link.tsx
+++ src/components/Main/Link.tsx
@@ -1,9 +1,11 @@
 import { Row } from "components/common/Div";
+import { Img } from "components/common/Img";
 import { Toast } from "components/common/Toast";
 import { useState } from "react";
 import styled from "styled-components";
 import { Palette } from "styles/Palette";
 import Typo from "styles/Typo";
+import chain from "assets/icons/chain.svg";
 
 export const Link = () => {
     const [toast, setToast] = useState<boolean>(false);
@@ -20,10 +22,10 @@ export const Link = () => {
             document.body.removeChild(textArea);
             setToast(true);
 
-            //3초 뒤 toast 팝업 삭제
+            //2초 뒤 toast 팝업 삭제
             setTimeout(() => {
                 setToast(false);
-            }, 3000);
+            }, 2000);
         } catch (error) {
             console.error("클립보드 복사 오류:", error);
         }
@@ -31,36 +33,38 @@ export const Link = () => {
 
     return (
         <Row gap={10}>
-            <LinkDiv>
-                <Typo.s3>https://from.anon/instaId</Typo.s3>
-            </LinkDiv>
+            <LinkAddress>
+                <Img src={chain} width={20} height={20} alt="chain img"></Img>
+                <Typo.s4 color={Palette.Gray60}>
+                    https://from.anon/instaId
+                </Typo.s4>
+            </LinkAddress>
             <Button onClick={handleClickButton}>
-                <Typo.s3>링크 복사</Typo.s3>
+                <Typo.s4 color={Palette.Gray60}>복사</Typo.s4>
             </Button>
             <Toast show={toast} text="링크가 복사되었습니다." />
         </Row>
     );
 };
 
-const LinkDiv = styled(Row)`
-    width: 200px;
-    height: 21px;
+const LinkAddress = styled(Row)`
+    width: 225px;
+    height: 29px;
     align-items: center;
     justify-content: flex-start;
 
-    padding: 0 11px;
+    padding: 0 5px;
+    gap: 5px;
 
-    border: 1px solid ${Palette.Mandarin};
-    border-radius: 5px;
-    background: ${Palette.White};
+    border-radius: 12px;
+    background: ${Palette.Gray05};
 `;
 const Button = styled(Row)`
-    width: 55px;
-    height: 21px;
+    width: 41px;
+    height: 29px;
     align-items: center;
     justify-content: center;
 
-    border-radius: 5px;
-    background: ${Palette.Mandarin};
-    color: ${Palette.White};
+    border-radius: 12px;
+    background: ${Palette.Gray20};
 `;
