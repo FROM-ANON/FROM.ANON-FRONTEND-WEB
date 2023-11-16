8131ba08e7ea3f6bb07f36d2ad7dcd037262c790
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-11-16 16:52:25 +0900

    fix: Main page UI 수정

--- src/components/Main/Menus.tsx
+++ src/components/Main/Menus.tsx
@@ -1,26 +1,21 @@
+import styled from "styled-components";
 import { Column } from "components/common/Div";
 import { Menu } from "./Menu";
-import Typo from "styles/Typo";
-import { Link } from "./Link";
 
 export const Menus = () => {
     return (
-        <Column>
-            <Menu to="/inbox">
-                <Typo.h3>받은 편지함</Typo.h3>
+        <Container>
+            <Menu to="/inbox">받은 편지함</Menu>
+            <Menu to="/" underComponent={true}>
+                편지함 링크 공유하기
             </Menu>
-            <Menu to="/">
-                <Column gap={7}>
-                    <Typo.h3>편지함 링크 공유하기</Typo.h3>
-                    <Link />
-                </Column>
-            </Menu>
-            <Menu to="/send">
-                <Typo.h3>편지 보내기</Typo.h3>
-            </Menu>
-            <Menu to="/mailpaper">
-                <Typo.h3>편지지</Typo.h3>
-            </Menu>
-        </Column>
+            <Menu to="/send">편지 보내기</Menu>
+            <Menu to="/mailpaper">편지지</Menu>
+        </Container>
     );
 };
+
+const Container = styled(Column)`
+    gap: 15px;
+    padding: 0 27px;
+`;
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,12 @@
-import { Column } from "components/common/Div";
+import { PageContainer } from "components/common/Div";
 import { Header } from "components/common/Header";
 import { Menus } from "../../components/Main/Menus";
 
 export const Main = () => {
     return (
-        <Column>
+        <PageContainer backgroundColor="var(--linear_gradient, linear-gradient(173.93deg,#F9DDAA 4.81%, #EDE2CD 100%))">
             <Header type="main"></Header>
             <Menus />
-        </Column>
+        </PageContainer>
     );
 };
