4639540d55f895a1fe0566efb03d8436672cffdf
asaei623 <86418308+asaei623@users.noreply.github.com>
2023-11-18 19:50:10 +0900

    fix: clickedMailListState 삭제

--- src/recoil/atom.ts
+++ src/recoil/atom.ts
@@ -1,17 +1,5 @@
 import { atom } from "recoil";
 
-export const clickedMailListState = atom({
-    key: "clickedMailListState",
-    default: {
-        mail: [
-            { isClicked: false },
-            { isClicked: false },
-            { isClicked: false },
-            { isClicked: false },
-        ],
-    },
-});
-
 export const alertOpenState = atom({
     key: "alertOpenState",
     default: {
