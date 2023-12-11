c25bc624a3cd15862f15ce32f6379a1fde859fa3
asaei623 <himtows623@gmail.com>
2023-12-11 20:50:53 +0900

    feat: 편지지 관련 api 연결

--- src/components/Mailpaper/MailpapersList.tsx
+++ src/components/Mailpaper/MailpapersList.tsx
@@ -5,46 +5,78 @@ import { Palette } from "styles/Palette";
 import favoriteNo from "assets/icons/favorite-no.svg";
 import favoriteYes from "assets/icons/favorite-yes.svg";
 import crown from "assets/icons/crown.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import {
+    delFavoriteMailPaperApi,
+    getAllFavoriteMailPapersApi,
+    getAllMailPapersApi,
+    postFavoriteMailPaperApi,
+} from "network/apis/mailPaperApis";
+import { useRecoilState } from "recoil";
+import { mailPaperState } from "recoil/atom";
+import { mailpaperType } from "types";
 
-export const MailpapersList = () => {
-    const [mailpaper, setMailpaper] = useState([
-        { id: 1, isPro: true, favorite: false },
-        { id: 2, isPro: false, favorite: true },
-        { id: 3, isPro: false, favorite: false },
-        { id: 4, isPro: true, favorite: true },
-        { id: 5, isPro: true, favorite: true },
-        { id: 6, isPro: false, favorite: false },
-        { id: 7, isPro: true, favorite: false },
-    ]);
+export const MailpapersList = ({ toggleMenu }: { toggleMenu: number }) => {
+    const [mailPaper, setMailPaper] = useRecoilState(mailPaperState);
+    const [toggledMPList, setToggledMPList] = useState<Array<mailpaperType>>(
+        []
+    );
+    const fetchAllMailPapers = async () => {
+        let res = await getAllMailPapersApi();
+        setMailPaper({ ...mailPaper, mailPaperList: res?.data });
+    };
+    const fetchAllFavoriteMailPapers = async () => {
+        let res = await getAllFavoriteMailPapersApi();
+        setMailPaper({ ...mailPaper, favoriteMailPaperList: res?.data });
+    };
+
     const mailpaperSet = [];
-    for (let i = 0; i < mailpaper.length; i += 2) {
-        const set = mailpaper.slice(i, i + 2);
+    for (let i = 0; i < toggledMPList?.length; i += 2) {
+        const set = toggledMPList.slice(i, i + 2);
         mailpaperSet.push(set);
     }
 
-    const handleClickFavorite = (id: number) => {
-        setMailpaper((prevMailpaper) => {
-            return prevMailpaper.map((item) => {
-                if (item.id === id) {
-                    return {
-                        ...item,
-                        favorite: !item.favorite,
-                    };
-                } else {
-                    return item;
-                }
-            });
-        });
+    const handleClickFavorite = async (mailPaperId: number) => {
+        const favoriteMailPaperId = mailPaper.favoriteMailPaperList.find(
+            (fmp) => fmp.mailPaperId === mailPaperId
+        )?.id;
+
+        console.log(mailPaperId);
+        console.log(favoriteMailPaperId);
+        if (favoriteMailPaperId) {
+            await delFavoriteMailPaperApi(favoriteMailPaperId);
+        } else {
+            await postFavoriteMailPaperApi(mailPaperId);
+        }
+        fetchAllFavoriteMailPapers();
     };
+    const checkIsFavorite = (mailPaperId: number): boolean => {
+        return mailPaper.favoriteMailPaperList.some(
+            (fmp) => fmp.mailPaperId === mailPaperId
+        );
+    };
+
+    useEffect(() => {
+        fetchAllMailPapers();
+        fetchAllFavoriteMailPapers();
+    }, []);
+    useEffect(() => {
+        if (toggleMenu === 0) {
+            setToggledMPList(mailPaper.mailPaperList);
+        } else {
+            setToggledMPList(
+                mailPaper.mailPaperList.filter((it) => checkIsFavorite(it.id))
+            );
+        }
+    }, [toggleMenu]);
 
     return (
         <Container>
             {mailpaperSet.map((set, setIdx) => (
                 <RowContainer key={setIdx}>
                     {set.map((item, index) => (
-                        <Mailpaper key={index}>
-                            {item.isPro ? (
+                        <Mailpaper key={index} mailPaperId={item.id}>
+                            {item.pro ? (
                                 <Img
                                     src={crown}
                                     width={11}
@@ -54,9 +86,12 @@ export const MailpapersList = () => {
                             ) : (
                                 <div></div>
                             )}
-
                             <Img
-                                src={item.favorite ? favoriteYes : favoriteNo}
+                                src={
+                                    checkIsFavorite(item.id)
+                                        ? favoriteYes
+                                        : favoriteNo
+                                }
                                 width={13}
                                 height={22}
                                 alt="favorite icon"
@@ -77,7 +112,7 @@ const Container = styled(Row)`
     gap: 24px;
     padding: 17px 0;
 `;
-const Mailpaper = styled(Row)`
+const Mailpaper = styled(Row)<{ mailPaperId: number }>`
     width: 145px;
     height: 145px;
     justify-content: space-between;
@@ -85,6 +120,11 @@ const Mailpaper = styled(Row)`
     padding: 16px;
     background: ${Palette.Gray10};
     border-radius: 25px;
+
+    background-image: ${(props) =>
+        `url("/mailPaper/${props.mailPaperId}.svg")`};
+    background-size: cover;
+    background-repeat: no-repeat;
 `;
 const RowContainer = styled(Row)`
     max-width: 314px;
--- src/pages/Mailpaper/index.tsx
+++ src/pages/Mailpaper/index.tsx
@@ -2,13 +2,29 @@ import { MailpapersList } from "components/Mailpaper/MailpapersList";
 import { Column } from "components/common/Div";
 import { Header } from "components/common/Header";
 import { ToggleMenu } from "components/common/ToggleMenu";
+import { useState } from "react";
+import { useRecoilState } from "recoil";
+import { mailPaperState } from "recoil/atom";
 
 export const Mailpaper = () => {
+    const [mailPaper, setMailPaper] = useRecoilState(mailPaperState);
+    const [toggleMenu, setToggleMenu] = useState<number>(0);
+    let mailPapers =
+        toggleMenu === 0
+            ? mailPaper.mailPaperList
+            : mailPaper.favoriteMailPaperList;
+
     return (
         <Column>
             <Header type="sub" text="편지지" />
-            {/* <ToggleMenu menu1={"전체"} menu2={"즐겨찾기"} /> */}
-            <MailpapersList></MailpapersList>
+            <ToggleMenu
+                menu1={"전체"}
+                menu2={"즐겨찾기"}
+                menu1Len={mailPaper.mailPaperList.length}
+                menu2Len={mailPaper.favoriteMailPaperList.length}
+                setToggleMenu={setToggleMenu}
+            />
+            <MailpapersList toggleMenu={toggleMenu}></MailpapersList>
         </Column>
     );
 };
