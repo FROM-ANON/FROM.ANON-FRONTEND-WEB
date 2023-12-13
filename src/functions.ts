0f99efb82a939876f8dea14fe4c0fc919f0571aa
asaei623 <himtows623@gmail.com>
2023-12-13 13:23:27 +0900

    feat: handleUnauthorizedAccess 함수 추가

--- src/functions.ts
+++ src/functions.ts
@@ -33,3 +33,7 @@ export const checkIsLogin = async () => {
         return false;
     }
 };
+export const handleUnauthorizedAccess = () => {
+    alert("로그인 후 접근 가능한 페이지입니다.");
+    window.location.href = "/main";
+};
