b407b35f3cf47185ad9f677fb40d7cdecb110b72
asaei623 <himtows623@gmail.com>
2023-12-13 12:35:03 +0900

    feat: checkIsLogin 함수 생성

--- src/functions.ts
+++ src/functions.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
+import { getUserByTokenApi } from "network/apis/userApis";
 
 export const handleError = (err: any) => {
     if (axios.isAxiosError(err)) {
         const status = err.response?.status;
         if (status !== undefined) {
-            console.log(err.response?.status);
+            console.log("Axios err:", err.response?.status);
 
             switch (status) {
                 case 500:
@@ -16,11 +17,19 @@ export const handleError = (err: any) => {
                 case 406:
                     return status;
                 default:
-                    alert("알 수 없는 오류가 발생했습니다.");
-                    break;
+                    return status;
             }
         }
     } else {
         console.log("Non-Axios error:", err);
+    }
+};
+export const checkIsLogin = async () => {
+    try {
+        await getUserByTokenApi();
+        //유저가 로그인한 상태
+        return true;
+    } catch (err) {
+        return false;
     }
 };
