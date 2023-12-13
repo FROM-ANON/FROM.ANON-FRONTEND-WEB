e8614d942a01d942b6d4df0775d89ed579efb0c7
asaei623 <himtows623@gmail.com>
2023-12-13 12:37:13 +0900

    fix: 로그인하지 않은 경우 favoriteMailPaper 관한 기능이 보이지 않도록 수정

--- src/pages/Mailpaper/index.tsx
+++ src/pages/Mailpaper/index.tsx
@@ -1,7 +1,9 @@
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { MailpapersList } from "components/Mailpaper/MailpapersList";
 import { Column } from "components/common/Div";
 import { Header } from "components/common/Header";
 import { ToggleMenu } from "components/common/ToggleMenu";
+import { checkIsLogin, handleError } from "functions";
 import {
     getAllFavoriteMailPapersApi,
     getAllMailPapersApi,
@@ -13,20 +15,40 @@ import { mailPaperState } from "recoil/atom";
 export const Mailpaper = () => {
     const [mailPaper, setMailPaper] = useRecoilState(mailPaperState);
     const [toggleMenu, setToggleMenu] = useState<number>(0);
+    const [isLogin, setIsLogin] = useState<boolean>();
+
+    useEffect(() => {
+        checkIsLogin().then((res) => setIsLogin(res));
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 let resAll = await getAllMailPapersApi();
-                let resFavorite = await getAllFavoriteMailPapersApi();
-                setMailPaper({
-                    ...mailPaper,
-                    mailPaperList: resAll?.data,
-                    favoriteMailPaperList: resFavorite?.data,
-                });
-                console.log(mailPaper);
+                let isErr = false;
+                let resFavorite;
+
+                try {
+                    resFavorite = await getAllFavoriteMailPapersApi();
+                } catch (err) {
+                    const status = handleError(err);
+                    if (status === 400) isErr = true;
+                }
+
+                if (!isErr) {
+                    setMailPaper({
+                        ...mailPaper,
+                        mailPaperList: resAll?.data,
+                        favoriteMailPaperList: resFavorite?.data,
+                    });
+                } else {
+                    setMailPaper({
+                        ...mailPaper,
+                        mailPaperList: resAll?.data,
+                    });
+                }
             } catch (err) {
-                console.log(err);
+                handleError(err);
             }
         };
         fetchData();
@@ -35,13 +57,15 @@ export const Mailpaper = () => {
     return (
         <Column>
             <Header type="sub" text="편지지" />
-            <ToggleMenu
-                menu1={"전체"}
-                menu2={"즐겨찾기"}
-                menu1Len={mailPaper.mailPaperList.length}
-                menu2Len={mailPaper.favoriteMailPaperList.length}
-                setToggleMenu={setToggleMenu}
-            />
+            {isLogin && (
+                <ToggleMenu
+                    menu1={"전체"}
+                    menu2={"즐겨찾기"}
+                    menu1Len={mailPaper.mailPaperList.length}
+                    menu2Len={mailPaper.favoriteMailPaperList.length}
+                    setToggleMenu={setToggleMenu}
+                />
+            )}
             <MailpapersList
                 key={mailPaper.mailPaperList.length}
                 toggleMenu={toggleMenu}
